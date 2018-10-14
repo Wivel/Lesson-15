0d2fa3392113aad19a138adbf42eba7e6352e9f6
Wivel <wiveloriginal@yandex.ru>
2018-10-14 20:38:28 +0300

    4 point

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
-function isNumberInRange(num) {
+function isNumberInRange(num) {  // Проверяет условие "0<число<10"
     if (num > 0 && num < 10) {
         return true;
     } else {
         return false;
     }
+}
+
+function getDigitsSum(num) {
+    let sum = 0;
+    var num = String(num);                            // Если заменить var на Let, то функция перестаёт работать ...
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+    return sum;
 }
